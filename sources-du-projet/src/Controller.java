f00f4712e55587171ea1b48aa9e184a21ca41621
yanis <yanis@DESKTOP-PDVLSQS>
2020-09-16 12:08:50 +0200

    New Label

--- sources-du-projet/src/Controller.java
+++ sources-du-projet/src/Controller.java
@@ -1,10 +1,18 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class Controller implements Initializable {
-
+	
+	@FXML	private Label nameFile;
+	@FXML	private Label NBfaces;
+	@FXML	private Label nameAuthor;
+	@FXML	private Label dateFile;
+	@FXML	private Label description;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
