fa3e595a5f02a3cbc3d7de58ae53fbcba07cd598
Quentin Gillot <quentingillot914@gmail.com>
2020-09-16 12:04:53 +0100

    "Vue FXML"

--- sources-du-projet/src/Main.java
+++ sources-du-projet/src/Main.java
@@ -1,7 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 
 	public static void main(String[] args) {
+		launch (args);
 		System.out.println("Test Commit");
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene (root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
