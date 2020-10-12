71d719eba7f9a1784180ac556c443b4b3b82dbaa
pipou <pipou@LAPTOP-NBDBR0AB>
2020-10-12 15:00:18 +0200

    Changements addPoints, getNBfaces, getNBsommets, ajout getNbLinesIntro, addFaces

--- sources-du-projet/src/main/java/application/Controller.java
+++ sources-du-projet/src/main/java/application/Controller.java
@@ -59,66 +59,17 @@ public class Controller implements Initializable{
 		listView.getSelectionModel().getSelectedItems().addListener(new openModel());
 		listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 		
-		root3D = new Group();
-		//pane = new Pane(root3D);
-		TriangleMesh pyramidMesh = new TriangleMesh();
-		File f = new File ("./airplane.ply");
-		TriangleMesh test= addPoints(f);
-		pyramidMesh.getTexCoords().addAll(0,0);
-		float h = 15;                    // Height
-		float s = 30;                    // Side
-		pyramidMesh.getPoints().addAll(
-		        0,    0,    0,            // Point 0 - Top
-		        0,    h,    -s/2,         // Point 1 - Front
-		        -s/2, h,    0,            // Point 2 - Left
-		        s/2,  h,    0,            // Point 3 - Back
-		        0,    h,    s/2           // Point 4 - Right
-		    );
-		pyramidMesh.getFaces().addAll(
-		        0,0,  2,0,  1,0,          // Front left face
-		        0,0,  1,0,  3,0,          // Front right face
-		        0,0,  3,0,  4,0,          // Back right face
-		        0,0,  4,0,  2,0,          // Back left face
-		        4,0,  1,0,  2,0,          // Bottom rear face
-		        4,0,  3,0,  1,0           // Bottom front face
-		    );
-		MeshView pyramid = new MeshView(pyramidMesh);
+		File f = new File ("./ledeatintinNB.ply");
+		TriangleMesh test = new TriangleMesh();
+		addPoints(f, test);
+		addFaces(f, test);
+		
+		MeshView pyramid = new MeshView(test);
 		pyramid.setDrawMode(DrawMode.FILL);
-		//pyramid.setMaterial(new PhongMaterial(Color.BLUE));
 		pyramid.setTranslateX(200);
 		pyramid.setTranslateY(100);
 		pyramid.setTranslateZ(200);
 		pane.getChildren().add(pyramid);
-		Box cube = new Box(1,1,1);
-		
-		PhongMaterial blueMaterial = new PhongMaterial();
-		blueMaterial.setDiffuseColor(Color.BLUE);
-		blueMaterial.setSpecularColor(Color.BLUE);
-		
-	//	root3D.getChildren().add(cube);
-		
-		PerspectiveCamera camera = new PerspectiveCamera(true);
-		Group cameraGroup = new Group(camera);
-		pane.getChildren().add(cameraGroup);
-		
-		Rotate ry = new Rotate();
-		ry.setAxis(Rotate.Y_AXIS);
-		ry.setAngle(-15);
-		
-		Translate tz = new Translate();
-		tz.setZ(-10);
-		tz.setY(-1);
-
-		cameraGroup.getTransforms().addAll(ry,tz);
-		
-		PointLight light = new PointLight(Color.WHITE);
-		light.setTranslateX(-20);
-		light.setTranslateY(-20);
-		light.setTranslateZ(-15);
-		light.getScope().addAll(pane);
-		pane.getChildren().add(light);
-		
-		
 	}
 	
 	//Event select task in listView
@@ -146,21 +97,19 @@ public class Controller implements Initializable{
 				
 			}
 		}
-		
+
 		public int getNbFaces (File f) throws IOException{
 			int nbLines=-1;
 			FileReader fr= new FileReader (pathRessources+f);
 			BufferedReader br = new BufferedReader(fr);
-			while (( br.readLine())!=null ) {
-				
-				if (br.readLine().substring(0,2).equals("3 "))
-					nbLines=nbLines+2;
+			String temp = "";
+			while((temp = br.readLine())!=null) {
+				if(temp.substring(0,2).equals("3 ")) nbLines ++;
 			}
 			fr.close();
 			return nbLines;
-			
 		}
-		
+
 		public int getNBSommets (File f) throws IOException{
 			int nbLines=-1;
 			FileReader fr = new FileReader (pathRessources+f);
@@ -169,27 +118,58 @@ public class Controller implements Initializable{
 				nbLines++;
 		}
 		fr.close();
-		return nbLines-10-getNbFaces(f);
+		return nbLines-getNbLineIntro(f)-getNbFaces(f);
 		
 		}
 		
-		public TriangleMesh addPoints ( File f)  {
-			TriangleMesh tri= new TriangleMesh();
+		public void addPoints(File f, TriangleMesh tr)  {
 			try {
 			FileReader fr = new FileReader(pathRessources+f);
 			BufferedReader br = new BufferedReader(fr);
-			for (int i=0;i<=8;i++)
-				br.readLine();
+			while(!br.readLine().equals("end_header"));
 			for (int j=0;j<getNBSommets(f)-1;j++) {
 				String ligne = br.readLine();
-				String [] tab=ligne.split(" ");
-				tri.getPoints().addAll(Float.parseFloat(tab [0]), Float.parseFloat(tab[1]), Float.parseFloat(tab[2]));
-				System.out.println(tab[0]+"/"+tab[1]+"/"+tab[2]);
+				String [] tab=ligne.split("   ");
+				for(int x = 0; x < tab.length; x++) {
+					tr.getPoints().addAll(Float.parseFloat(tab [x]));
+				}
 			}
 			fr.close();
 			}catch (IOException e) {
 				e.printStackTrace();
 			}
-			return tri;
+		}
+		
+		public void addFaces(File f, TriangleMesh tr)  {
+			try {
+			FileReader fr = new FileReader(pathRessources+f);
+			BufferedReader br = new BufferedReader(fr);
+			while(!br.readLine().equals("end_header"));
+			for(int i = 0; i < getNBSommets(f); i++) {
+				br.readLine();
+			}
+			for (int j = 0; j < getNbFaces(f) - 1; j++) {
+				String ligne = br.readLine();
+				String [] tab = ligne.split(" ");
+				for(int x = 0; x < tab.length; x++) {
+					tr.getFaces().addAll(Integer.parseInt(tab [x]));
+					System.out.print(tab[x]);
+				}
+				System.out.println();
+			}
+			fr.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public int getNbLineIntro(File f) throws IOException {
+			int nbLines = 0;
+			FileReader fr = new FileReader (pathRessources + f);
+			BufferedReader br = new BufferedReader(fr);
+			while(!br.readLine().equals("end_header")) {
+				nbLines ++;
+			}
+			return nbLines;
 		}
 }
