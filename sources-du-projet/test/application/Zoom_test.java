bbfe9c1ed9315d6766f205b54eb57af95b8e8441
yanis <yanis.vroland@gmail.com>
2020-11-06 18:38:26 +0100

    edit zoomTest

--- sources-du-projet/test/application/Zoom_test.java
+++ sources-du-projet/test/application/Zoom_test.java
@@ -1,5 +1,36 @@
 package application;
 
-public class Zoom_test {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Zoom_test {
+	private Controller control;
+
+	@BeforeEach
+	public void init() throws IOException {
+		control.initFaces(new File("./ressources/ledeatintinNB.ply"));
+		
+	}
+	
+	@Test
+	public void zoom() throws IOException {
+		Controller tmp = control;
+		control.zoomOnModel();
+		for (int i = 0; i < control.listeSommets.size(); i++) {
+			assertEquals(tmp.listeSommets.get(i).getX()*1,control.listeSommets.get(i).getX());
+			assertEquals(tmp.listeSommets.get(i).getY()*1,control.listeSommets.get(i).getY());
+			assertEquals(tmp.listeSommets.get(i).getZ()*1,control.listeSommets.get(i).getZ());
+		}
+	}
+	
+	@Test
+	public void deZoom() {
+		
+	}
+	
 }
