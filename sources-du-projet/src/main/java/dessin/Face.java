15116c925351675d486e2ddcfc9b37610ec17de1
Admin <Admin@Pc-Fixe-Matheo>
2021-01-07 19:37:21 +0100

    Javadoc Face.java

--- sources-du-projet/src/main/java/dessin/Face.java
+++ sources-du-projet/src/main/java/dessin/Face.java
@@ -2,21 +2,70 @@ package dessin;
 
 import java.util.ArrayList;
 
+/**
+ * Face est la classe pour les Faces du modèle.
+ * <p> Une Face est caractérisée par :
+ * <ul><li>Une liste de d'entiers.</li></ul></p>
+ * @author Antoine Delaby, Yanis Vroland, Quentin Gillot, Mathéo Gallego
+ */
 public class Face {
-	private ArrayList<Integer>sommets;
+	/**
+	 * La liste des Sommets de la Face.
+	 * @see Face#getSommets()
+	 */
+	private ArrayList<Integer> sommets;
 	
+	/**
+	 * Constructeur Face.
+	 * <p> A la construction d'un objet Face, la liste des Sommets est créée vide.</p>
+	 */
 	public Face() {
 		this.sommets = new ArrayList<Integer>();
 	}
 	
+	/**
+	 * Ajoute un Sommet à la liste des Sommets de la Face.
+	 * @param s
+	 * 			L'id du Sommet du fichier ply à ajouter à la Face.
+	 */
 	public void addSommet(int s) {
-		this.sommets.add(s);
+		sommets.add(s);
 	}
 
+	/**
+	 * Retourne la liste des id des Sommets de la Face.
+	 * @return les id des Sommets de la Face, sous forme d'une liste d'entiers.
+	 */
 	public ArrayList<Integer> getSommets() {
 		return sommets;
 	}
-	
+
+	/**
+	 * Retourne l'id du 1er Sommet de la liste des Sommets de la Face.
+	 * @return L'id du 1er Sommet de la liste des Sommets de la Face, sous forme d'entier.
+	 */
+	public int getS1() {
+		return sommets.get(0);
+	}
+	/**
+	 * Retourne l'id du 2e Sommet de la liste des Sommets de la Face.
+	 * @return L'id du 2e Sommet de la liste des Sommets de la Face, sous forme d'entier.
+	 */
+	public int getS2() {
+		return sommets.get(1);
+	}
+	/**
+	 * Retourne l'id du 3e Sommet de la liste des Sommets de la Face.
+	 * @return L'id du 3e Sommet de la liste des Sommets de la Face, sous forme d'entier.
+	 */
+	public int getS3() {
+		return sommets.get(2);
+	}
+
+	/**
+	 * Retourne une chaine de caractères contenant les id des Sommets de la Face.
+	 * @return Les id des Sommet de la Face [id1, id2, id3, ...] séparées par des virgules, sous la forme d'une chaine de caractère.
+	 */
 	public String toString() {
 		String res = "Face : [" + this.sommets.get(0);
 		for(int i = 1; i < this.sommets.size(); i++) {
@@ -25,20 +74,4 @@ public class Face {
 		res += "]";
 		return res;
 	}
-
-	public int getS1() {
-		// TODO Auto-generated method stub
-		return sommets.get(0);
-	}
-	
-	public int getS2() {
-		// TODO Auto-generated method stub
-		return sommets.get(1);
-	}
-	
-	public int getS3() {
-		// TODO Auto-generated method stub
-		return sommets.get(2);
-	}
-	
 }
