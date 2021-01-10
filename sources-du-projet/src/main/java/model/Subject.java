9d436cdefe56dda5778821905170f35e63199e8f
Admin <Admin@Pc-Fixe-Matheo>
2021-01-10 18:58:50 +0100

    Javadoc Subject.java

--- sources-du-projet/src/main/java/model/Subject.java
+++ sources-du-projet/src/main/java/model/Subject.java
@@ -6,33 +6,63 @@ import java.util.List;
 import controller.Controller;
 import vues.Observer;
 
+/**
+ * Subject est la classe abstraite de gestion des Sujets (les Observés).
+ * <p> Un Subject est caractérisé par l'information suivante :
+ * <ul><li>Une liste de Controlleurs, suceptible d'être changée.</ul></li></p>
+ * @author Antoine Delaby, Yanis Vroland, Quentin Gillot, Mathéo Gallego
+ */
 public abstract class Subject {
+	/**
+	 * La liste des Controllers du Sujet.
+	 * @see Subject#getAttached()
+	 */
     protected List<Controller> attached;
 
+    /**
+     * Constructeur Subject.
+     * <li>Créé une nouvelle liste de Controllers, vide.</li>
+     */
     public Subject() {
         attached = new ArrayList<>();
     }
     
-    public List<Controller> getAttached() {
-		return attached;
-	}
-
+    /**
+     * Ajoute à la Liste des Controlleurs du Sujet l'observeur passé en paramètre.
+     * @param obs
+     * 				L'observeur que l'on souhaite ajouter à la liste des Controlleurs du Sujet.
+     */
 	public void attach(Observer obs) {
         if (! attached.contains( obs)) {
             attached.add((Controller) obs);
         }
     }
-
+	
+	/**
+     * Retire à la Liste des Controlleurs du Sujet l'observeur passé en paramètre.
+     * @param obs
+     * 				L'observeur que l'on souhaite retirer de la liste des Controlleurs du Sujet.
+     */
     public void detach(Observer obs) {
         if (attached.contains( obs)) {
             attached.remove(obs);
         }
     }
     
+    /**
+     * Met à jour tous les Observers du Sujet.
+     */
 	public void notifyForUpdate() {
 		for(Observer c : attached) {
 			c.update();
 		}
 	}
 
+    /**
+     * Retourne la liste des Controllers du Sujet.
+     * @return La liste des Controllers du Sujet, sous forme de List de Controllers.
+     */
+    public List<Controller> getAttached() {
+		return attached;
+	}
 }
