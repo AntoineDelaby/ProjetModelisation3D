496bf730c4295f9ac92a124c27a764f163002331
Admin <Admin@Pc-Fixe-Matheo>
2021-01-10 17:27:38 +0100

    Javadoc Vue.java

--- sources-du-projet/src/main/java/vues/Vue.java
+++ sources-du-projet/src/main/java/vues/Vue.java
@@ -5,8 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+/**
+ * La Vue est la classe permettant l'affichage de la Scène.
+ * De plus, Vue hérite de la classe Controller.
+ * @author Antoine Delaby, Yanis Vroland, Quentin Gillot, Mathéo Gallego
+ */
 public class Vue extends Controller{
-		
+
+	/**
+	 * Constructeur Vue.
+	 * <p>Va instancier une nouvelle Scène avec un fichier fxml du nom de la ressource passée en paramètre.</p>
+	 * @param vue
+	 * 				Le nom de la ressource du fichier fxml.
+	 */
 	public Vue(String vue) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource(vue));
