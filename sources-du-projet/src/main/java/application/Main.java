0a1206a647d0d391762f3b855f414ad087d4981a
Admin <Admin@Pc-Fixe-Matheo>
2021-01-10 17:16:01 +0100

    Javadoc Main.java

--- sources-du-projet/src/main/java/application/Main.java
+++ sources-du-projet/src/main/java/application/Main.java
@@ -3,12 +3,27 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import vues.Vue;
 
+/**
+ * Le Main.
+ * <p><ul><li>Appelé en premier au démarrage de l'application.</li>
+ * <li>Va créer la scène et la vue.</li></ul></p>
+ * @author Antoine Delaby, Yanis Vroland, Quentin Gillot, Mathéo Gallego
+ */
 public class Main extends Application {
+	
+	/**
+	 * Va Créer la Scène.
+	 */
 	@Override
 	public void start(Stage primaryStage) {
 		new Vue("Vue.fxml");
 	}
 	
+	/**
+	 * Va Démarrer la Scène
+	 * @param args
+	 * 				Paramètres de démarrage de la Scène.
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
